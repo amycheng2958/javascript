c23b813d84c0247aeaf257c4842cbd1f71f3642a
chengyunyun <790958649@qq.com>
2021-05-19 22:05:07 +0800

    1

---
+++ lodash.js
@@ -0,0 +1,7 @@
+var _ = require('lodash');
+var arr = ['xiaoming','xiaofang','xiaozhang','xiaoli'];
+
+console.log(_.first(arr))
+console.log(_.forEach(arr))
+console.log(_.reverse(arr))
+console.log(arr)
